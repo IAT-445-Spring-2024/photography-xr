bed15d88195733b522d435fa21207554cca9d260
Jenkins Xu <yongqix@sfu.ca>
2024-03-18 21:57:02 -0700

    Copy tree instance rotation.

--- Assets/Scripts/TreeReplacer.cs
+++ Assets/Scripts/TreeReplacer.cs
@@ -11,9 +11,10 @@ public class TreeReplacer : MonoBehaviour {
         TerrainData terrainData = terrain.GetComponent<Terrain>().terrainData;
         foreach (TreeInstance treeInstance in terrainData.treeInstances) {
             Vector3 treeWorldPosition = Vector3.Scale(treeInstance.position, terrainData.size) + Terrain.activeTerrain.transform.position;
-            Instantiate(treePrefab, treeWorldPosition, Quaternion.identity);
+            GameObject tree = Instantiate(treePrefab, treeWorldPosition, Quaternion.identity);
+            tree.transform.SetParent(terrain.transform);
             Quaternion rotation = Quaternion.Euler(0, treeInstance.rotation * Mathf.Rad2Deg, 0);
-            treePrefab.transform.rotation = rotation;
+            tree.transform.rotation = rotation;
         }
 
         terrainData.treeInstances = new TreeInstance[0];
